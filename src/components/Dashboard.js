56f096bc47ec1a1d42f27fb3076945cbecc72b73
Viet Van Do <dvxdxm@gmail.com>
2024-11-04 07:15:19 +0700

    add css

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,18 +1,76 @@
 import { connect } from "react-redux";
-import Tweet from "./Tweet";
+import { Fragment } from "react";
 
 const Dashboard = (props) => {
   return (
-    <div>
-      <h3 className="center">Your Timeline</h3>
-      <ul className="dashboard-list">
-        {/* {props.tweetIds.map((id) => (
-          <li key={id}>
-            <Tweet id={id} />
-          </li>
-        ))} */}
-      </ul>
-    </div>
+    <Fragment>
+      <div class="section">
+        <div class="section-title">New Questions</div>
+        <div class="card-container">
+          <div class="card">
+            <div class="card-body">
+              <div class="username">mtsamis</div>
+              <div class="timestamp">4:11 PM | 11/23/2021</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <div class="username">sarahedo</div>
+              <div class="timestamp">5:22 PM | 3/3/2017</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <div class="username">tylermcginnis</div>
+              <div class="timestamp">6:42 AM | 12/24/2016</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <div class="username">sarahedo</div>
+              <div class="timestamp">10:21 PM | 6/28/2016</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+        </div>
+      </div>
+      <div class="section">
+        <div class="section-title">Done</div>
+        <div class="card-container">
+          <div class="card">
+            <div class="card-body">
+              <div class="username">tylermcginnis</div>
+              <div class="timestamp">6:42 AM | 12/24/2016</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <div class="username">tylermcginnis</div>
+              <div class="timestamp">6:42 AM | 12/24/2016</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <div class="username">tylermcginnis</div>
+              <div class="timestamp">6:42 AM | 12/24/2016</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <div class="username">tylermcginnis</div>
+              <div class="timestamp">6:42 AM | 12/24/2016</div>
+            </div>
+            <button class="show-btn">Show</button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
